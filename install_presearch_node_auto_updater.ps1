c12efbf130eb9773cd453324b446ecd0ff635437
vladoklimo <77233918+vladoklimo@users.noreply.github.com>
2021-01-10 18:17:17 +0100

    initial release

---
+++ install_presearch_node_auto_updater.ps1
@@ -0,0 +1,105 @@
+### Author: Vladimir Klimo
+### 
+### This is installation script to inicialize Windows Scheduled Task for node updater
+### 
+### Please be aware that I do not take any responsibility for your actions with this script.
+### Use at your own risk. No guarantees.
+###
+
+###
+### Few configuration things before you execute anythng
+###
+
+# name of the folder on C:\ driver, where script download the node updater script
+$folderName = "presearch_node_updater"
+
+###
+### Configuration done
+###
+
+### After this line, edit at your own risk
+
+
+$installFolder = "C:\" + $folderName
+$installFile = $installFolder + "\" + "preserach_node_check.ps1"
+
+$githubURL = "https://raw.githubusercontent.com/vladoklimo/presearch-node-auto-updater/main/presearch_node_check.ps1"
+
+function checkFile {
+	[CmdletBinding()]
+	param ([Parameter(Mandatory)] $file)
+	
+	return Test-Path $file -PathType leaf
+}
+
+function checkFolder {
+	[CmdletBinding()]
+	param ([Parameter(Mandatory)] $folder)
+	
+	return Test-Path $folder
+}
+
+function install_presearch_node_updater {
+	[CmdletBinding()]
+	param ([boolean]$Force)
+
+	if ( (checkFolder -folder $installFolder) -and -not($Force) ) {
+		#do some stuff
+		Write-Host "Already installed ?? Nothing to do (maybe not)"
+	}
+	else {
+		Write-Host "Not installed ... so starting up"
+		
+		if (checkFolder -folder $installFolder) {
+			Write-Host "$installFolder already exists ... skipping create folder routine"
+		}
+		else {
+			Write-Host "Creating folder $installFolder"
+			New-Item -Path "C:\" -Name $folderName -ItemType "directory" | out-null
+		}
+		
+		Write-Host 'Starting downloading the GitHub Repository'
+		Invoke-RestMethod -Uri $githubURL -OutFile $installFile
+		Write-Host 'Download finished'
+		
+		$job_action = New-ScheduledTaskAction -Execute $installFile -WorkingDirectory "$installFolder"
+		
+		if ($job_action -eq $null) {
+			Write-Host "Cannot inicialize task action parameter"
+			return
+		}
+		
+		$job_trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RandomDelay (New-TimeSpan -Minutes 10) -RepetitionInterval (New-TimeSpan -Minutes 10)
+		
+		if ($job_trigger -eq $null) {
+			Write-Host "Cannot inicialize task trigger parameter"
+			return
+		}
+		
+		$job_settings = New-ScheduledTaskSettingsSet -Hidden -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -RunOnlyIfNetworkAvailable
+		
+		if ($job_settings -eq $null) {
+			Write-Host "Cannot inicialize task settings parameter"
+			return
+		}
+		
+		$task = New-ScheduledTask -Action $job_action -Trigger $job_trigger -Settings $job_settings
+		
+		if ($task -eq $null) {
+			Write-Host "Cannot inicialize task for registration" 
+			return
+		}
+		
+		$registration = Register-ScheduledTask -TaskName "Presearch Node Auto-Updater" -InputObject $task
+		
+		if ($registration -eq $null) {
+			Write-Host "Cannot register Scheduled task - Presearch Node Auto-Updater" 
+			return
+		}
+		else {
+			Write-Host "Created recurring task to check presearch node for an updates. Scheduled Task Name: Presearch Node Auto-Updater"
+		}
+	}
+}
+
+install_presearch_node_updater -Force $true
---
+++ presearch_node_check.ps1
@@ -0,0 +1,58 @@
+### Author: Vladimir Klimo
+### 
+### This is a simple auto-updater for presearch node running under Windows version of Docker
+### As early releases of nodes's (beta)versions does not handle everything flawlessly.
+### I was looking for a more comfortable way of handling node's updates.
+### So this scripts is something like a small program which checks docker's logs on local machine.
+### If it finds out a specific error statement "Nodes version is too old" then program executes the
+### update procedure and start the node again, but with latest version of node itself.
+### 
+### Please be aware that I do not take any responsibility for your actions with this script.
+### Use at your own risk. No guarantees.
+###
+
+###
+### Few configuration things before you execute anythng
+###
+# set the name of docker container, presearch is using "presearch.node" as default name, so be it
+$container_name = "presearch.node"
+# set the registration code for your node, this can be obtained from https://nodes.presearch.org/dashboard after you login to your account
+$presearch_registration_code = "<PUT-YOUR-PRESEARCH-CODE-HERE>"
+
+###
+### Configuration done
+###
+
+### After this line, edit at your own risk
+
+function Check-Presearch-Node-Version {
+    $oldNodeIndicator = docker container logs $container_name  --since 10m | Select-String  -Pattern "Node version is too old" -SimpleMatch | measure | % { $_.Count }
+	If ($oldNodeIndicator -gt 0) {
+		Write-Host "Checking status: Node version is too old."
+		Write-Host "Stopping the docker $container_name container"
+		#now stop to container & cleanup the old one
+		docker container stop $container_name | out-null
+		docker container rm $container_name | out-null
+		
+		# update image to the latest version
+		Write-Host "Updating node image to latest version"
+		docker image pull presearch/node:latest | out-null
+		
+		# run the node
+		Write-Host "Starting node with"
+		$container_id = docker run -dt -v presearch-node-storage:/app/node --restart=unless-stopped --network  host --name $container_name -e REGISTRATION_CODE=$presearch_registration_code presearch/node
+		
+		# finishing
+		Write-Host "Container started ... checking logs"
+		for ($i = 10; $i -gt 0; $i--) {
+			Start-Sleep -Seconds 1
+			Write-Host -NoNewline "...$i"
+		} 
+		docker container logs $container_id
+	}
+	else {
+		Write-Host "Nothing to do"
+	}
+}
+
+Check-Presearch-Node-Version
